a3651fdd756cf9d6379bbd9cebdb34c7be382c90
Alex Koppel <alex@alexkoppel.com>
2018-06-14 18:35:32 -0500

    Add Utils / transformObject

---
+++ test/Utils_test.js
@@ -0,0 +1,59 @@
+import { transformObject } from "../source/Utils.js";
+
+describe("Utils", () => {
+  describe("transformObject", () => {
+    let sampleObject;
+
+    beforeEach(() => {
+      sampleObject = {
+        ctor: "TestMessage",
+        _0: "a primitive value",
+        _1: {
+          type: "record",
+          containing: {
+            ctor: "AnObject",
+            _0: "SomeValue"
+          }
+        },
+        _2: {
+          ctor: "AnotherObject"
+        }
+      };
+    });
+
+    it("transforms the entry according to the block provided", () => {
+      const transformation = entry => {
+        const result = {};
+        Object.keys(entry).forEach(key => {
+          if (key == "ctor") {
+            result.constructor = entry.ctor;
+          } else {
+            result[key.toUpperCase() + "processed"] = transformObject(
+              entry[key],
+              transformation
+            );
+          }
+        });
+
+        return result;
+      };
+
+      expect(transformObject(sampleObject, transformation)).toEqual({
+        constructor: "TestMessage",
+        _0processed: "a primitive value",
+        _1processed: {
+          // records are handled differently than objects
+          type: "record",
+          containing: {
+            // this shows that embedded objects are also processed
+            constructor: "AnObject",
+            _0processed: "SomeValue"
+          }
+        },
+        _2processed: {
+          constructor: "AnotherObject"
+        }
+      });
+    });
+  });
+});
